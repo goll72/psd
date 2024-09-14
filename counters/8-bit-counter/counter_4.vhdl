c6a23768483869446aa4c32cc4324bf72bfdcd47
goll <joaog.bastos@protonmail.ch>
2024-09-13 23:51:43 -0300

    Adicionar makefile e testes, corrigir formatação do código

--- counters/8-bit-counter/counter_4.vhdl
+++ counters/8-bit-counter/counter_4.vhdl
@@ -5,14 +5,14 @@ use ieee.std_logic_1164.all;
 
 library work;
 
-entity counter_8 is
+entity counter_4 is
     port (clk, enable, clr : in  std_logic;
-                         q : out std_logic_vector(7 downto 0));
+                         q : out std_logic_vector(3 downto 0));
 end entity;
 
 architecture structural of counter_8 is
-    signal i : std_logic_vector(6 downto 0);
-    SIGNAL s : std_logic_vector(7 downto 0);
+    signal i : std_logic_vector(2 downto 0);
+    SIGNAL s : std_logic_vector(3 downto 0);
 BEGIN
     t_0 : entity work.ff_t(structural) port map (
         clk => clk,
@@ -40,34 +40,6 @@ BEGIN
         clr => clr,
         t => i(2),
         q => s(3)
-    );
-    i(3) <= s(3) and i(2);
-    t_4 : entity work.ff_t(structural) port map (
-        clk => clk,
-        clr => clr,
-        t => i(3),
-        q => s(4)
-    );
-    i(4) <= s(4) and i(3);
-    t_5 : entity work.ff_t(structural) port map (
-        clk => clk,
-        clr => clr,
-        t => i(4),
-        q => s(5)
-    );
-    i(5) <= s(5) and i(4);
-    t_6 : entity work.ff_t(structural) port map (
-        clk => clk,
-        clr => clr,
-        t => i(5),
-        q => s(6)
-    );
-    i(6) <= s(6) and i(5);
-    t_7 : entity work.ff_t(structural) port map (
-        clk => clk,
-        clr => clr,
-        t => i(6),
-        q => s(7)
     );
     q <= s;
 end architecture;
