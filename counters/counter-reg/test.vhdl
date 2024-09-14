cf035606ee3c29abe5d68c3923af1029b91355dd
goll <joaog.bastos@protonmail.ch>
2024-09-14 13:11:24 -0300

    Consertar indentação

--- counters/counter-reg/test.vhdl
+++ counters/counter-reg/test.vhdl
@@ -11,36 +11,36 @@ end entity;
 use std.env.stop;
 
 architecture behavioral of test is
-	signal clk : std_logic := '0';
-	signal enable, clr : std_logic;
-	signal count : std_logic_vector(15 downto 0);
+    signal clk : std_logic := '0';
+    signal enable, clr : std_logic;
+    signal count : std_logic_vector(15 downto 0);
 
-	constant CLK_PERIOD : time := 100 ps;
+    constant CLK_PERIOD : time := 100 ps;
 
-	attribute keep : boolean;
-	attribute keep of clk, enable, clr : signal is true;
+    attribute keep : boolean;
+    attribute keep of clk, enable, clr : signal is true;
 begin
-	main : entity work.counter(behavioral) port map (
-		clk => clk,
-		enable => enable,
-		clr => clr,
-		q => count
-	);
+    main : entity work.counter(behavioral) port map (
+        clk => clk,
+        enable => enable,
+        clr => clr,
+        q => count
+    );
 
-	clk <= not clk after CLK_PERIOD / 2;
+    clk <= not clk after CLK_PERIOD / 2;
 
-	stim : process is
-	begin
-		clr <= '0';
-		wait for CLK_PERIOD;
-		clr <= '1';
-		enable <= '1';
-		wait for 12 * CLK_PERIOD;
-		enable <= '0';
-		wait for 5 * CLK_PERIOD;
-		enable <= '1';
-		wait for 5 * CLK_PERIOD;
+    stim : process is
+    begin
+        clr <= '0';
+        wait for CLK_PERIOD;
+        clr <= '1';
+        enable <= '1';
+        wait for 12 * CLK_PERIOD;
+        enable <= '0';
+        wait for 5 * CLK_PERIOD;
+        enable <= '1';
+        wait for 5 * CLK_PERIOD;
 
-		stop;
-	end process;
+        stop;
+    end process;
 end architecture;
