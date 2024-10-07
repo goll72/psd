a9945e5a182d85044f442103ef35774b508cc9d0
goll <joaog.bastos@protonmail.ch>
2024-10-07 15:41:39 -0300

    sample: python init infra

---
+++ init.py
@@ -0,0 +1,36 @@
+import sys
+import shutil
+
+from datetime import datetime
+from pathlib import Path
+
+now = datetime.now()
+now_str = now.strftime("%H:%M:%S %B %d, %Y")
+
+project_path = Path(sys.argv[1])
+
+project_name = project_path.name
+
+project_path.mkdir()
+
+def copy_template_file(f, g):
+    text = f.read()
+
+    text = (
+        text
+            .replace("%DATE%", now_str)
+            .replace("%PROJECT%", project_name)
+    )
+
+    g.write(text)
+
+sample_path = Path("sample")
+
+with open(sample_path / "project.qpf") as f, open(project_path / f"{project_name}.qpf", "w") as g:
+    copy_template_file(f, g)
+
+with open(sample_path / "project.qsf") as f, open(project_path / f"{project_name}.qsf", "w") as g:
+    copy_template_file(f, g)
+
+shutil.copy(sample_path / "Makefile", project_path)
+shutil.copy(sample_path / "main.vhdl", project_path)
