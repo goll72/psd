98fa643c415b39cfd8b0a118dbce52601fceb3dc
goll <joaog.bastos@protonmail.ch>
2024-10-13 19:48:54 -0300

    fsm/morse: remover código desnecessário (latch/ff)

--- fsm/morse/util/shift_reg.vhdl
+++ fsm/morse/util/shift_reg.vhdl
@@ -29,20 +29,3 @@ begin
     
     q <= s;
 end architecture;
-
-architecture structural of shift_reg is
-    signal s : std_logic_vector(N - 1 downto 0);
-begin
-    ff_d_chain : for i in 0 to N - 1
-    generate
-        ff : entity work.ff_d(structural) port map (
-            clk => clk,
-            d => s((i - 1) mod N),
-            preset => not (load and data(i)),
-            clr => not (load and not data(i)),
-            q => s(i)
-        );
-    end generate;
-    
-    q <= s;
-end architecture;
