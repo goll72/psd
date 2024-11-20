3aecd804ee22f93468f2f433926b7225d9fc7a25
goll <joaog.bastos@protonmail.ch>
2024-11-20 14:05:00 -0300

    demos/prime.s: consertar comportamento do código para números "negativos"

--- cpu/demos/prime.s
+++ cpu/demos/prime.s
@@ -1,5 +1,5 @@
-wait
 ## Saída: 0 se for composto, 1 se for primo
+wait
 in a
 
 cmp a, 2
@@ -9,40 +9,53 @@ cmp a, 3
 jeq end_prime
 
 and a, 1
-## A instrução acima já fará o equivalente disso:
-# mov r, 0
+# A instrução acima já fará o equivalente a "mov r, 0"
 jeq end_composite
 
-## Novamente, temos um "mov r, 0" implícito
+# Novamente, temos um "mov r, 0" implícito
 sub a, 1
 jeq end_composite
 
-## Guarda o valor de r no endereço de memória 0xff
-store a, 0xff
+# Vamos verificar realizando divisões a partir do 3
 mov b, 3
 
 prime_check_loop:
-        ## Restaura r para verificarmos se r é divisível por b
-        ## b vai variar de 3 a n - 1
-        load r, 0xff
+        # Restaura r para verificarmos se r é divisível por b
+        # b vai variar de 3 a n - 1
+        mov r, a
+
+        # Se o número de entrada n for tratado como negativo em complemento
+        # de dois (MSB igual a 1), devemos subtrair b sucessivamente até
+        # que o MSB deixe de ser 1 para que possamos aplicar o algoritmo
+        cmp r, 0
+
+        jgr sub_mod_loop
+        jmp divide_sub_loop
+
+sub_mod_loop:
+        sub r, b
+
+        # Se o resultado for 0, r é divisível por b, logo é composto
+        jeq end_composite
+        # O resultado ainda é negativo quando interpretado em
+        # complemento de dois, devemos continuar subtraindo
+        jgr sub_mod_loop
 
 divide_sub_loop:
         sub r, b
 
-        ## Se o resultado for 0, r é divisível por b, logo é composto
+        # Se o resultado for 0, r é divisível por b, logo é composto
         jeq end_composite
-        ## Se for negativo, r não é divisível por b e devemos sair do loop
+        # Se for negativo, r não é divisível por b e devemos sair do loop
         jgr end_divide_sub_loop
-        ## Devemos continuar no loop
+        # Devemos continuar no loop
         jmp divide_sub_loop
 end_divide_sub_loop:
         add b, 1
         mov b, r
 
-        load r, 0xff
-
-        ## Chegamos a n - 1 (acabamos de incrementar b)
-        cmp b, r
+        # Já verificamos se n é divisível por n - 1 (acabamos de incrementar b)
+        cmp b, a
 
         jeq end_prime
         jmp prime_check_loop
